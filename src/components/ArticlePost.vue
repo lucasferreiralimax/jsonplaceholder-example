9a7939c3101296d5d40b2cb5025914569e73a1c5
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-11 15:52:51 -0300

    refactory winprovit with articlePost component

---
+++ src/components/ArticlePost.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  post: Object
+})
+</script>
+
+<template>
+  <article class="post" v-if="post">
+    <h2 class="title">Title: {{ post.title }}</h2>
+    <div class="info">
+      <p><b>Author:</b> {{ post.name }}</p>
+      <p><b>Email:</b> {{ post.email }}</p>
+      <p><b>Company:</b> {{ post.company.name }}</p>
+    </div>
+    <p class="content">{{ post.body }}</p>
+  </article>
+</template>
+
+<style lang="scss">
+.post {
+  margin: 2rem 0;
+  padding: 2rem;
+  border: 1px solid var(--color-border);
+  border-radius: 10px;
+  .title { text-transform: capitalize; }
+  .info {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 1rem;
+    @media (max-width: 1024px) {
+      flex-direction: column;
+    }
+    b {
+      font-weight: bold;
+      color: hsla(160, 100%, 37%, 1);
+    }
+    p { margin-right: 1rem; }
+  }
+  .content {
+    background: var(--color-border);
+    padding: 1rem;
+    border-radius: 10px;
+  }
+}
+</style>
--- src/components/Winprovit.vue
+++ src/components/Winprovit.vue
@@ -2,6 +2,7 @@
 import { reactive, ref, watch  } from 'vue'
 import { getUserList, getPostList } from "@/services/Winprovit"
 import ErrorHandler from './ErrorHandler.vue';
+import ArticlePost from './ArticlePost.vue';
 
 let usersData = ref([]);
 let postsData = ref([]);
@@ -19,7 +20,7 @@ const mergeUserPost = (posts, users) => {
     arrayMerge.push(Object.assign({}, post, userWithEqualId));
   })
 
-  console.log('result', arrayMerge);
+  console.log('Result', arrayMerge);
   postsResult.value = arrayMerge;
 }
 
@@ -50,41 +51,5 @@ watch([usersData, postsData], () => {
 
 <template>
   <ErrorHandler :errors="resultError" />
-  <article class="post" v-for="post in postsResult">
-    <h2 class="title">Title: {{ post.title }}</h2>
-    <div class="info">
-      <p><b>Author:</b> {{ post.name }}</p>
-      <p><b>Email:</b> {{ post.email }}</p>
-      <p><b>Company:</b> {{ post.company.name }}</p>
-    </div>
-    <p class="content">{{ post.body }}</p>
-  </article>
+  <ArticlePost v-for="post of postsResult" :post="post" />
 </template>
-
-<style lang="scss">
-.post {
-  margin: 2rem 0;
-  padding: 2rem;
-  border: 1px solid var(--color-border);
-  border-radius: 10px;
-  .title { text-transform: capitalize; }
-  .info {
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 1rem;
-    @media (max-width: 1024px) {
-      flex-direction: column;
-    }
-    b {
-      font-weight: bold;
-      color: hsla(160, 100%, 37%, 1);
-    }
-    p { margin-right: 1rem; }
-  }
-  .content {
-    background: var(--color-border);
-    padding: 1rem;
-    border-radius: 10px;
-  }
-}
-</style>

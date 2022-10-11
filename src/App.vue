03afd90772c0dbcfff816ea1037e3dbf3e1dd753
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-11 17:50:37 -0300

    add show version

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Winprovit from './components/Winprovit.vue'
+import pkg from '../package.json';
 </script>
 
 <template>
   <header>
     <img alt="Winprovit logo" class="logo winprovit" src="./assets/winprovit.png" width="150" height="70" />
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    Version {{ pkg.version }}
   </header>
 
   <main>

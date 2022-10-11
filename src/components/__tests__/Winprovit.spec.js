e067aa1fd92adfb1ed700649d1ce43cac00f4f02
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-11 17:35:54 -0300

    test(unit): adjustment validation posts page with separate

--- src/components/__tests__/Winprovit.spec.js
+++ src/components/__tests__/Winprovit.spec.js
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import { mount, flushPromises } from '@vue/test-utils';
 import mergeUserPost from '@/utils/mergeUserPost';
+import separatePosts from '@/utils/separatePosts'
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
@@ -8,9 +9,10 @@ import Winprovit from '../Winprovit.vue';
 import { users, posts } from '../__mocks__/ServicesWinprovit';
 
 const mockPosts = mergeUserPost(posts, users);
+const mockPostsSeparate = separatePosts(mockPosts, 10);
 
-async function sleep(durée){
-  return new Promise((r) => setTimeout(r, durée));
+async function sleep(time){
+  return new Promise((r) => setTimeout(r, time));
 }
 
 const mockedServer = setupServer(
@@ -40,21 +42,21 @@ describe('Winprovit', () => {
   it('renders component list posts', async () => {
     expect(wrapper.find('.posts')).toBeDefined();
     expect(wrapper.findAll('.post')).toBeDefined();
-    expect(wrapper.findAll('.post').length).toEqual(100);
+    expect(wrapper.findAll('.post').length).toEqual(10);
   })
 
   it('renders virtual data list posts', async () => {
-    expect(wrapper.vm.postsResult.length).toEqual(100);
-    expect(wrapper.vm.postsResult).toEqual(mockPosts);
+    expect(wrapper.vm.postsResult.length).toEqual(10);
+    expect(wrapper.vm.postsResult).toEqual(mockPostsSeparate);
   })
 
-  for(let [index, post] of mockPosts.entries()) {
+  for(let index = 0; index < mockPostsSeparate.length; index++) {
     it('render content post', async () => {
-      expect(wrapper.vm.postsResult[index].title).toEqual(post.title);
-      expect(wrapper.vm.postsResult[index].name).toEqual(post.name);
-      expect(wrapper.vm.postsResult[index].email).toEqual(post.email);
-      expect(wrapper.vm.postsResult[index].company.name).toEqual(post.company.name);
-      expect(wrapper.vm.postsResult[index].body).toEqual(post.body);
+      expect(wrapper.vm.postsResult[0][index].title).toEqual(mockPostsSeparate[0][index].title);
+      expect(wrapper.vm.postsResult[0][index].name).toEqual(mockPostsSeparate[0][index].name);
+      expect(wrapper.vm.postsResult[0][index].email).toEqual(mockPostsSeparate[0][index].email);
+      expect(wrapper.vm.postsResult[0][index].company.name).toEqual(mockPostsSeparate[0][index].company.name);
+      expect(wrapper.vm.postsResult[0][index].body).toEqual(mockPostsSeparate[0][index].body);
     })
   }
 })
